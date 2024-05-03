a8f04df49f9fb79d892695be88cad559948a6c93
AlanMartinezDev <alanmartiola@gmail.com>
2024-05-03 12:56:41 +0200

    Create PropiedadController

---
+++ controllers/PropiedadController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Controllers;
+
+class PropiedadController
+{
+    public static function index()
+    {
+        echo "desde admin index";
+    }
+
+    public static function crear()
+    {
+        echo "desde admin crear";
+    }
+
+    public static function actualizar()
+    {
+        echo "desde admin actualizar";
+    }
+}
