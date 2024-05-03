db3a5175fb32dff7368b0a9490e22efb7e82a2bb
AlanMartinezDev <alanmartiola@gmail.com>
2024-05-03 14:48:27 +0200

    Admin page

--- views/propiedades/admin.php
+++ views/propiedades/admin.php
@@ -1,2 +1,46 @@
-<h1>Admin</h1>
-<p>Desde la vista</p>
+<main class="contenedor seccion">
+    <h1>Administrador de TarracoLuxe</h1>
+    <?php
+    if ($resultado) {
+        $mensaje = mostrarNotificacion(intval($resultado));
+        if ($mensaje) { ?>
+            <p class="alerta exito"><?php echo s($mensaje); ?></p>
+    <?php
+        }
+    }
+    ?>
+
+    <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
+    <a href="/admin/vendedores/crear.php" class="boton boton-amarillo">Nuevo vendedor</a>
+
+    <h2>Propiedades</h2>
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($propiedades as $propiedad) : ?>
+                <tr>
+                    <td><?php echo $propiedad->id; ?></td>
+                    <td><?php echo $propiedad->titulo; ?></td>
+                    <td><img src="/imagenes/<?php echo $propiedad->imagen; ?>" alt="<?php echo $propiedad->titulo; ?>" class="imagen-tabla"></td>
+                    <td><?php echo $propiedad->precio; ?> €</td>
+                    <td>
+                        <a href="propiedades/actualizar.php?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">Actualizar</a>
+                        <form method="post" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $propiedad->id; ?>">
+                            <input type="hidden" name="tipo" value="propiedad">
+                            <input type="submit" value="Borrar" class="boton-rojo-block">
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</main>
